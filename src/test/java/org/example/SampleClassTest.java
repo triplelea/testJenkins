bdd5cba35f52a57b99f3c57ed92a3b2c1f38f815
triplelea <121679427+triplelea@users.noreply.github.com>
2023-01-24 15:36:15 +0100

    remove one test

--- src/test/java/org/example/SampleClassTest.java
+++ src/test/java/org/example/SampleClassTest.java
@@ -11,11 +11,11 @@ class SampleClassTest {
         SampleClass sampleClass = new SampleClass(CHOSEN_NUMBER);
         assertEquals(CHOSEN_NUMBER,sampleClass.getNumber());
     }
-    @Test
-    void newSampleTest(){
-        final int CHOSEN_NUMBER = 5;
-        SampleClass sampleClass = new SampleClass(3);
-        assertEquals(CHOSEN_NUMBER,sampleClass.getNumber());
-    }
+//    @Test
+//    void newSampleTest(){
+//        final int CHOSEN_NUMBER = 5;
+//        SampleClass sampleClass = new SampleClass(3);
+//        assertEquals(CHOSEN_NUMBER,sampleClass.getNumber());
+//    }
 
 }
