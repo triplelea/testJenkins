af0d376e28e336b15ab188358b7d7fdf28baeb05
triplelea <121679427+triplelea@users.noreply.github.com>
2023-01-24 14:20:13 +0100

    add on test

---
+++ src/main/java/org/example/SampleClass.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class SampleClass {
+    private int number;
+
+    public SampleClass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
---
+++ src/test/java/org/example/SampleClassTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleClassTest {
+    @Test
+    void sampleTest(){
+        final int CHOSEN_NUMBER = 5;
+        SampleClass sampleClass = new SampleClass(CHOSEN_NUMBER);
+        assertEquals(CHOSEN_NUMBER,sampleClass.getNumber());
+    }
+
+}
